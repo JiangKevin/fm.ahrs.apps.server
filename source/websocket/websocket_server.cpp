bd54051f46274cc47a216d1058689d8983a31a14
kevin <jiangxingkai@fmbj.com.cn>
2025-04-29 12:23:34 +0800

    备份一下

--- source/websocket/websocket_server.cpp
+++ source/websocket/websocket_server.cpp
@@ -1,76 +1,96 @@
 #include "websocket_server.hpp"
 #include <iostream>
+#include <chrono>
 
-WebSocketServer::WebSocketServer() : ioc_( 1 ), running_( false ), acceptor_( ioc_, tcp::endpoint( tcp::v4(), 0 ) ) {}
+WebSocketServer::WebSocketServer() : ioc_(1), running_(false), acceptor_(ioc_, tcp::endpoint(tcp::v4(), 0)) {}
 
-WebSocketServer::~WebSocketServer()
-{
+WebSocketServer::~WebSocketServer() {
     stop();
 }
 
-void WebSocketServer::setPort( const std::string& port )
-{
+void WebSocketServer::setPort(const std::string& port) {
     port_ = port;
     acceptor_.close();
-    acceptor_.open( tcp::v4() );
-    acceptor_.set_option( net::ip::tcp::acceptor::reuse_address( true ) );
-    acceptor_.bind( tcp::endpoint( tcp::v4(), static_cast< unsigned short >( std::stoi( port_ ) ) ) );
+    acceptor_.open(tcp::v4());
+    acceptor_.set_option(net::ip::tcp::acceptor::reuse_address(true));
+    acceptor_.bind(tcp::endpoint(tcp::v4(), static_cast<unsigned short>(std::stoi(port_))));
     acceptor_.listen();
 }
 
-void WebSocketServer::start()
-{
-    if ( ! running_ )
-    {
-        running_      = true;
-        serverThread_ = std::thread( &WebSocketServer::acceptConnections, this );
+void WebSocketServer::start() {
+    if (!running_) {
+        running_ = true;
+        serverThread_ = std::thread([this]() {
+            acceptConnections();
+            sendPeriodicMessages();
+        });
     }
 }
 
-void WebSocketServer::stop()
-{
-    if ( running_ )
-    {
+void WebSocketServer::stop() {
+    if (running_) {
         running_ = false;
         ioc_.stop();
-        if ( serverThread_.joinable() )
-        {
+        if (serverThread_.joinable()) {
             serverThread_.join();
+        }
+        {
+            std::lock_guard<std::mutex> lock(connectionsMutex_);
+            for (auto conn : connections_) {
+                try {
+                    conn->close(websocket::close_code::normal);
+                    delete conn;
+                } catch (...) {}
+            }
+            connections_.clear();
         }
     }
 }
 
-void WebSocketServer::acceptConnections()
-{
-    try
-    {
-        while ( running_ )
-        {
-            tcp::socket socket{ ioc_ };
-            acceptor_.accept( socket );
-            std::thread( &WebSocketServer::handleConnection, this, websocket::stream< tcp::socket >( std::move( socket ) ) ).detach();
+void WebSocketServer::acceptConnections() {
+    try {
+        while (running_) {
+            tcp::socket socket{ioc_};
+            acceptor_.accept(socket);
+            auto ws = new websocket::stream<tcp::socket>(std::move(socket));
+            {
+                std::lock_guard<std::mutex> lock(connectionsMutex_);
+                connections_.push_back(ws);
+            }
+            std::thread([this, ws]() {
+                handleConnection(std::move(*ws));
+            }).detach();
         }
-    }
-    catch ( const std::exception& e )
-    {
+    } catch (const std::exception& e) {
         std::cerr << "Server main exception: " << e.what() << std::endl;
     }
 }
 
-void WebSocketServer::handleConnection( websocket::stream< tcp::socket > ws )
-{
-    try
-    {
+void WebSocketServer::handleConnection(websocket::stream<tcp::socket> ws) {
+    try {
         ws.accept();
         beast::flat_buffer buffer;
-        ws.read( buffer );
-        std::string message = beast::buffers_to_string( buffer.data() );
-        std::cout << "Server received: " << message << std::endl;
-        ws.write( net::buffer( "Server received: " + message ) );
-        ws.close( websocket::close_code::normal );
-    }
-    catch ( const std::exception& e )
-    {
+        while (running_) {
+            ws.read(buffer);
+            std::string message = beast::buffers_to_string(buffer.data());
+            std::cout << "Server received: " << message << std::endl;
+            buffer.consume(buffer.size());
+        }
+    } catch (const std::exception& e) {
         std::cerr << "Server connection exception: " << e.what() << std::endl;
+    }
+}
+
+void WebSocketServer::sendPeriodicMessages() {
+    while (running_) {
+        {
+            std::lock_guard<std::mutex> lock(connectionsMutex_);
+            for (auto conn : connections_) {
+                try {
+                    conn->write(net::buffer("Periodic message from server"));
+                } catch (...) {}
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 }
--- source/websocket/websocket_server.hpp
+++ source/websocket/websocket_server.hpp
@@ -4,32 +4,37 @@
 #include <boost/asio.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
-#include <string>
 #include <thread>
+#include <string>
+#include <vector>
+#include <mutex>
 
-namespace beast     = boost::beast;
-namespace http      = beast::http;
+namespace beast = boost::beast;
+namespace http = beast::http;
 namespace websocket = beast::websocket;
-namespace net       = boost::asio;
-using tcp           = net::ip::tcp;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
 
-class WebSocketServer
-{
+class WebSocketServer {
 public:
     WebSocketServer();
     ~WebSocketServer();
-    void setPort( const std::string& port );
+    void setPort(const std::string& port);
     void start();
     void stop();
+
 private:
     void acceptConnections();
-    void handleConnection( websocket::stream< tcp::socket > ws );
+    void handleConnection(websocket::stream<tcp::socket> ws);
+    void sendPeriodicMessages();
 
-    std::string     port_;
+    std::string port_;
     net::io_context ioc_;
-    tcp::acceptor   acceptor_;
-    std::thread     serverThread_;
-    bool            running_;
+    tcp::acceptor acceptor_;
+    std::thread serverThread_;
+    bool running_;
+    std::vector<websocket::stream<tcp::socket>*> connections_;
+    std::mutex connectionsMutex_;
 };
 
 #endif
